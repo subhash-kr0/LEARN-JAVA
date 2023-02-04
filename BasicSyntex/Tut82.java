fb793c251c7a7d9da34a5e69dfe9baa02142df91
Subhashkr1 <subhashkr855@gmail.com>
2023-02-04 17:21:14 +0530

    New Commit

--- BasicSyntex/Tut82.java
+++ BasicSyntex/Tut82.java
@@ -1,3 +1,35 @@
+// Ducking Exception Using throws keyword in Java
+
+class MyException extends Exception{
+    public MyException(String s){
+        super(s);
+    }
+}
+
+class A{
+    public void show() throws MyException{
+        System.out.println("In A");
+        throw new MyException("This is my exception");
+    }
+
+    public void show1() throws MyException{
+        System.out.println("In A");
+        throw new MyException("This is my exception");
+    }
+}
+
 public class Tut82 {
-    
+    public static void main(String[] args) throws MyException {
+        A a = new A();
+        try{
+            a.show();
+        }
+        catch(Exception e){
+            System.out.println("Exception Handled");
+        }
+
+        A b = new A();
+        b.show1();
+        b.show();
+    }
 }
